41f115b045e4aab96c2a7dc25b55f71cbcb593f3
s green <shaunhunnewell@outlook.com>
2021-11-02 13:11:38 -0400

    update
    less server lag

--- client.lua
+++ client.lua
@@ -1,1 +1,20 @@
-test
+Citizen.CreateThread(function()
+    while true do
+      Citzen.Wait(0) --must have every tick 
+
+      -- Traffic and ped density nanagement (these things need to run every tick)
+      SetTrafficDensity(0.0) --0.0= none. 1.0 = Max
+      SetPedDensity(0.0) ----0.0= none. 1.0 = Max
+    end
+end)
+
+function SetTrafficDensity(density)
+     SetParkedVehicleDensityMultiplierThisFrame(density)
+     SetVehicleDensityMultiplierThisFrame(density)
+     SetRandomVehicleDensityMultiplierThisFrame(density)
+end
+
+function SetPedDensity(density)
+     SetPedDensityMultiplierThisFrame(density)
+     SetScenarioPedDensityMultiplierThisFrame(density)
+end
