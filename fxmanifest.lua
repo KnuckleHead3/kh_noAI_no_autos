4615e889edcac0808b9ad6f39f0f16457a978a2e
s green <shaunhunnewell@outlook.com>
2021-11-02 15:09:41 -0400

    updat
    updat all

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,27 +1,9 @@
 fx_version 'cerulean'
 games { 'gta5' }
 
+author 'Shaun Green'
+description 'A script to reduce or remove AI and server lag'
+version '1.0.0'
 
-ui_page 'html/ui.html'
 
-
-files {
-	'html/ui.html',
-  'html/main.0355962e.chunk.css',
-  'html/fonts/*.ttf',
-	'html/*.js'
-}
-
-client_scripts {
-  'client/cl_*.lua',
-  "@PolyZone/client.lua",
-  "@PolyZone/ComboZone.lua",
-}
-
-shared_script {
-  'shared/sh_*.*',
-}
-
-server_scripts {
-  'server/sv_*.lua',
-}
+client_scripts 'client,lua'
